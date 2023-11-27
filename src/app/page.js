c3e13ab3d8788f394c7e0455ac8fdb06d101b44a
osohyun0224 <osohyun0224@naver.com>
2023-11-28 02:59:06 +0900

    ✨ feat: 메인 페이지 인트로 / 버튼 컴포넌트 구현
    - 메인 페이지에서 인트로 텍스트를 구현하였습니다.
    - 메인 페이지에서 필요한 버튼 컴포넌트를 구현하였습니다.
    - 주어진 조건에 맞추어 버튼의 3가지 상태를 구현해두었습니다.

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
+import Button from '../components/Button'
 
-export default function Home() {
+export default function Main() {
+  const cities = ['Seoul', 'Tokyo', 'Paris', 'London'];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    <main className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.welcome}>Welcome to</span>
+        <span className={styles.appName}>Weather App!</span>
+      </h1>
+      <p className={styles.description}>
+        Choose a city from the list below to check the weather.
+      </p>
+      <div className={styles.buttonsContainer}>
+        {cities.map(city => <Button key={city} text={city} />)}
       </div>
     </main>
-  )
+  );
 }
