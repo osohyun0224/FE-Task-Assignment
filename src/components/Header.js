0a52d8ab86493a701935a318caaf0833e01c1e1b
osohyun0224 <osohyun0224@naver.com>
2023-11-28 16:00:34 +0900

    ✨ feat: 헤더 컴포넌트 누락 데이터 추가 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import styles from './Header.module.css';
 import Image from 'next/image';
 
-const Header = ({ date, location, population, temperature, feelsLike }) => {
+const Header = ({ date, location, population, temperature, feelsLike, weatherDescription, windSpeed, humidity }) => {
   return (
     <div className={styles.header}>
       <div className={styles.weatherIcon}>
@@ -18,10 +17,15 @@ const Header = ({ date, location, population, temperature, feelsLike }) => {
         <p className={styles.date}>{date}</p>
         <h2 className={styles.location}>{location}</h2>
         <p className={styles.population}>{`(인구수: ${population})`}</p>
+        <p className={styles.weatherDescription}>{weatherDescription}</p>
       </div>
       <div className={styles.temperature}>
         {temperature}℃
-        <p className={styles.feelsLike}>{feelsLike}</p>
+        <div className={styles.weatherDetails}>
+          <p className={styles.feelsLike}>{feelsLike}</p>
+          <p className={styles.additionalInfo}>{windSpeed}</p>
+          <p className={styles.additionalInfo}>{humidity}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Seoul.js
+++ src/pages/Seoul.js
@@ -3,15 +3,16 @@ import Header from '../components/Header';
 import Image from 'next/image';
 import styles from './Seoul.module.css';
 import axios from 'axios';
-
 export default function Seoul() {
   const [weatherData, setWeatherData] = useState({
     date: '',
     location: '',
-    population: '10000000',
+    population: '9776000',
     temperature: '',
     feelsLike: '',
     weatherDescription: '',
+    windSpeed: '',
+    humidity: '',
   });
 
   useEffect(() => {
@@ -23,20 +24,20 @@ export default function Seoul() {
         const newWeatherData = {
           date: new Date(data.dt * 1000).toLocaleString(),
           location: data.name,
-          population: '10000000',
+          population: '9776000',
           temperature: data.main.temp.toFixed(1),
           feelsLike: `Feels like ${data.main.feels_like.toFixed(1)}℃`,
           weatherDescription: data.weather[0].description,
+          windSpeed: `풍속 ${data.wind.speed} m/s `,
+          humidity: `습도 ${data.main.humidity}% `,
         };
         setWeatherData(newWeatherData);
       } catch (error) {
         console.error("날씨 데이터를 불러오지 못했습니다.", error);
       }
     };
-
     fetchWeather();
   }, []);
-
   return (
     <div className={styles.pageContainer}>
       <div className={styles.imageContainer}>
